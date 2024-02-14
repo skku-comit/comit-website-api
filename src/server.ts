487b7c861aea943fe0966e23030211f566bfb484
toothless8392 <hsp5235@naver.com>
2024-02-14 17:44:30 +0900

    add ts

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/api", (req: Request, res: Response, next: NextFunction) =>
+{
+    res.send("Hello");
+});
+
+app.listen("3000", () =>
+{
+    console.log("Server listening on port 3000");
+});
